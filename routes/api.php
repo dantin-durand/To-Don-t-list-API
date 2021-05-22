596758d7ea39b7cc8ef5e0b56fb09645bbceddcd
Dantin Durand <durand.dantin.pro@gmail.com>
2021-05-22 17:08:25 +0200

    mise en place du projet

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\ApiTokenController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+Route::post('auth/register', [ApiTokenController::class, 'register']);
+Route::post('auth/login', [ApiTokenController::class, 'login']);
+Route::middleware('auth:sanctum')->post('auth/me', [ApiTokenController::class, 'me']);
+Route::middleware('auth:sanctum')->post('auth/logout', [ApiTokenController::class, 'logout']);
